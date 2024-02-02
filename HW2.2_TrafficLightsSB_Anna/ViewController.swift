eb42bab20e92de242f300ff9045ca6ab737af21a
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-03 02:10:55 +0300

    Initial Commit

---
+++ HW2.2_TrafficLightsSB_Anna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.2_TrafficLightsSB_Anna
+//
+//  Created by Анна on 03.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
